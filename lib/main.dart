70e001c33b55a4078678b6397b896219f2a6d198
Nada A <nadayunuseng@gmail.com>
2026-06-02 14:27:13 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -162,28 +162,53 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  // void setupFCM() async {
+  //   // 🔔 Ask permission (important for Android 13+ / iOS)
+  //   await FirebaseMessaging.instance.requestPermission();
+
+  //   final notificationService = NotificationService();
+
+  //   // 📱 Initial Sync
+  //   notificationService.syncToken();
+
+  //   // 🔄 Listen for token refresh and re-register
+  //   FirebaseMessaging.instance.onTokenRefresh.listen((newToken) {
+  //     debugPrint("FCM TOKEN REFRESHED: $newToken");
+  //     notificationService.syncToken();
+  //   });
+
+  //   // 📩 Listen for messages (when app is open)
+  //   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //     debugPrint("Notification Received!");
+  //     debugPrint(message.notification?.title);
+  //     debugPrint(message.notification?.body);
+  //   });
+  // }
+
   void setupFCM() async {
-    // 🔔 Ask permission (important for Android 13+ / iOS)
-    await FirebaseMessaging.instance.requestPermission();
+  final settings = await FirebaseMessaging.instance.requestPermission();
 
-    final notificationService = NotificationService();
+  debugPrint("Permission Status: ${settings.authorizationStatus}");
 
-    // 📱 Initial Sync
+  String? token = await FirebaseMessaging.instance.getToken();
+
+  debugPrint("FCM TOKEN => $token");
+
+  final notificationService = NotificationService();
+
+  notificationService.syncToken();
+
+  FirebaseMessaging.instance.onTokenRefresh.listen((newToken) {
+    debugPrint("FCM TOKEN REFRESHED => $newToken");
     notificationService.syncToken();
+  });
 
-    // 🔄 Listen for token refresh and re-register
-    FirebaseMessaging.instance.onTokenRefresh.listen((newToken) {
-      debugPrint("FCM TOKEN REFRESHED: $newToken");
-      notificationService.syncToken();
-    });
-
-    // 📩 Listen for messages (when app is open)
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      debugPrint("Notification Received!");
-      debugPrint(message.notification?.title);
-      debugPrint(message.notification?.body);
-    });
-  }
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    debugPrint("Notification Received!");
+    debugPrint(message.notification?.title);
+    debugPrint(message.notification?.body);
+  });
+}
 
   @override
   Widget build(BuildContext context) {
